a66862f720065c96c58653f4a005a6254f81a9fa
taybro23 <taybro343@outlook.com>
2021-03-12 16:28:09 +0000

    added second map in restaurants section.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -7,3 +7,13 @@ function initMap() {
     zoom: 10,
   });
 }
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map2"), {
+    center: { 
+        lat: 53.244062017380976,
+        lng: -2.51970882939482
+    },
+    zoom: 10,
+  });
+}
