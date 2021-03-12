3b7e1647c8a9788a62aa670331b45c3252f60252
taybro23 <taybro343@outlook.com>
2021-03-12 22:29:31 +0000

    added commented code for js incase needed instead of jquery.

--- assets/javascript/places.js
+++ assets/javascript/places.js
@@ -1,4 +1,56 @@
 
+/*
+onclick="fombtn1()"
+
+var toggled = false
+
+function fombtn1() {
+    if (!toggled) {
+        toggled = true;
+        document.getElementById("fomt1").style.display = "none";
+        return;
+    }
+    if (toggled) {
+        toggled = false;
+        document.getElementById("fomt1").style.display = "block";
+        return;
+    }
+}
+
+
+onclick="fombtn2()"
+
+function fombtn2() {
+    if (!toggled) {
+        toggled = true;
+        document.getElementById("fomt2").style.display = "none";
+        return;
+    }
+    if (toggled) {
+        toggled = false;
+        document.getElementById("fomt2").style.display = "block";
+        return;
+    }
+}
+
+
+onclick="fombtn3()"
+
+function fombtn3() {
+    if (!toggled) {
+        toggled = true;
+        document.getElementById("fomt3").style.display = "none";
+        return;
+    }
+    if (toggled) {
+        toggled = false;
+        document.getElementById("fomt3").style.display = "block";
+        return;
+    }
+}
+
+*/
+
 $("#fombtn1").click(function(){
    $("#fomt1").toggle(); 
 });
