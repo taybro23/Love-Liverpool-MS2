824eaba6ba3aa44f928f827c8cbb5c8e2bbda49f
taybro23 <taybro343@outlook.com>
2021-03-12 16:22:53 +0000

    added google map functionality to the page. edited js file and scripts on index, added css for size of map on page.

---
+++ assets/javascript/script.js
@@ -0,0 +1,9 @@
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { 
+        lat: 53.244062017380976,
+        lng: -2.51970882939482
+    },
+    zoom: 10,
+  });
+}
