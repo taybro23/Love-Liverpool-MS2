daed8bf18a5d7f425fa5b603b84d61a67e7c8cf8
taybro23 <taybro343@outlook.com>
2021-03-14 16:36:43 +0000

    changed the jquery to javascript for the places section. onclick function works.

--- assets/javascript/places.js
+++ assets/javascript/places.js
@@ -1,7 +1,3 @@
-
-/*
-onclick="fombtn1()"
-
 var toggled = false
 
 function fombtn1() {
@@ -17,9 +13,6 @@ function fombtn1() {
     }
 }
 
-
-onclick="fombtn2()"
-
 function fombtn2() {
     if (!toggled) {
         toggled = true;
@@ -33,30 +26,31 @@ function fombtn2() {
     }
 }
 
-
-onclick="fombtn3()"
-
 function fombtn3() {
     if (!toggled) {
         toggled = true;
         document.getElementById("fomt3").style.display = "none";
+        document.getElementById("fomt4").style.display = "none";
         return;
     }
     if (toggled) {
         toggled = false;
         document.getElementById("fomt3").style.display = "block";
+        document.getElementById("fomt4").style.display = "block";
         return;
     }
 }
 
-*/
 
+/*
 $("#fombtn1").click(function(){
    $("#fomt1").toggle(); 
 });
 $("#fombtn2").click(function(){
    $("#fomt2").toggle(); 
 });
+
 $("#fombtn3").click(function(){
    $(".fomt3").toggle(); 
 });
+*/
