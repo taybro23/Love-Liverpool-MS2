27a36475fd9abcd63be989f6b8c526c6c9515755
taybro23 <taybro343@outlook.com>
2021-03-14 17:33:25 +0000

    added alert for form when submitted or failed in js.

--- assets/javascript/sendEmail.js
+++ assets/javascript/sendEmail.js
@@ -5,8 +5,10 @@ window.onload = function() {
                 .then(function() {
                     console.log('SUCCESS!');
                     document.getElementById("contact-form").reset();
+                    alert("Form submitted, a member of our team will get back to you shortly!");
                 }, function(error) {
                     console.log('FAILED.', error);
+                    alert("Form failed to submit, please try again.")
                 });
             });
         };
