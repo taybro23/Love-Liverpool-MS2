92fb5dd63c75449c449e11448363d583bd262a3c
taybro23 <taybro343@outlook.com>
2021-03-15 17:16:54 +0000

    changed the name of js file places.js to script.js, and amended html. added buttons in shopping section above map and moved shops list to above map. js added to make the buttons functional.

---
+++ assets/javascript/script.js
@@ -0,0 +1,18 @@
+$("#fombtn1").click(function(){
+   $("#fomt1").toggle(); 
+});
+$("#fombtn2").click(function(){
+   $("#fomt2").toggle(); 
+});
+$("#fombtn3").click(function(){
+   $(".fomt3").toggle(); 
+});
+
+
+
+
+
+
+$("#shopbtn").click(function(){
+    $(".shop-li").toggle();
+})
