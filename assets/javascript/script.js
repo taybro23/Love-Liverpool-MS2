2143eb5d676a73d2c3c32c040fa049732158274c
taybro23 <taybro343@outlook.com>
2021-03-22 20:15:52 +0000

    added opacity on scroll for the navbar

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -10,6 +10,14 @@ btnScroll.addEventListener("click", function () {
     });
 });
 
+// navbar scroll fade, walkthrough from youtube video - https://www.youtube.com/watch?v=xYA2bcEHKg8&t=570s
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+        $("nav").toggleClass("scrolled", $(this).scrollTop() > 100);
+    });
+});
+
 // places of interest buttons, walkthrough for jquery from CI used // 
 
 $(document).ready(function (){
