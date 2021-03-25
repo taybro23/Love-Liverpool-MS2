11963063745e4657509131506271402f23bd1ee1
taybro23 <taybro343@outlook.com>
2021-03-25 20:20:28 +0000

    content added for all places to stay markers.

--- assets/javascript/maps.js
+++ assets/javascript/maps.js
@@ -34,84 +34,86 @@ var stayMarkers = [
             lat: 53.40174798063911, 
             lng: -2.9710356966759215
         },
-        content: "<p>hope street hotel</p>"
+        content: '<a href="https://www.hopestreethotel.co.uk/" target="_blank"><h5 class="underline">Hope Street Hotel</h5></a> <h6>40 Hope St, Liverpool, L1 9DA</h6> <p>Tel: 0151 709 3000</p>'
+
     },
     {
         location: {
             lat: 53.40852776861474,  
             lng: -2.993437507530301
         },
-        content: "<p>hotel indigo</p>"
+        content: '<a href="https://www.hotelindigo.com/hotels/gb/en/liverpool/lplil/hoteldetail?fromRedirect=true&qSrt=sBR&qIta=99603195&icdv=99603195&glat=SEAR&qSlH=LPLIL&setPMCookies=true&qSHBrC=IN&qDest=10%20Chapel%20Street,%20Liverpool,%20GB&dp=true&gclid=CjwKCAjw6fCCBhBNEiwAem5SO3vrFwAfV58j6r58Ylu7_GwPeWPP2NkXyFX7DVngDZuGrMWowwL0ChoCR94QAvD_BwE&cid=58070&srb_u=1" target="_blank"><h5 class="underline">Hotel Indigo Liverpool</h5></a> <h6>10 Chapel St, Liverpool, L3 9AG</h6> <p>Tel: 0151 559 0111</p>'
     },
     {
         location: {
             lat: 53.3960674774933,  
             lng: -2.990690925356584
         },
-        content: "<p>hotel pullman</p>"
+        content: '<a href="https://www.guestreservations.com/pullman-liverpool/booking?gclid=CjwKCAjw6fCCBhBNEiwAem5SO09yZqz71ACK_CJgnpMrHLosVU-nnpr46AxHJhAcHisMzdQp6xLI-BoCEncQAvD_BwE" target="_blank"><h5 class="underline">Hotel Pullman Liverpool</h5></a> <h6>Kings Dock, Port of Liverpool, Liverpool, L3 4FP</h6> <p>Tel: 0151 945 1000</p>'
     },
     {
         location: {
             lat: 53.39890782385358, 
             lng: -2.989918449120226
         },
-        content: "<p>jurys inn liverpool</p>"
+        content: '<a href="https://www.jurysinns.com/hotels/liverpool?utm_medium=cpc&gclid=CjwKCAjw6fCCBhBNEiwAem5SOzWuGSULNITc6RZ2cBAGCxiWfEXUNs54fXdTxrw6dkbO6NoM3hiJohoCdXoQAvD_BwE&gclsrc=aw.ds" target="_blank"><h5 class="underline">Jurys Inn Liverpool</h5></a> <h6>31 Keel Wharf, Liverpool, L3 4FN</h6> <p>Tel: 0151 244 3777</p>'
     },
     {
         location: {
             lat: 53.40860451485431, 
             lng: -2.9973428041275647
         },
-        content: "<p>malmaison liverpool</p>"
+        content: '<a href="https://www.malmaison.com/locations/liverpool/" target="_blank"><h5 class="underline">Malmaison Liverpool</h5></a> <h6>7 William Jessop Way, Liverpool, L3 1QZ</h6> <p>Tel: 0151 363 3640</p>'
+
     },
     {
         location: {
             lat: 53.408077483082764, 
             lng: -2.9820831303575934
         },
-        content: "<p>marriott</p>"
+        content: '<a href="https://www.marriott.co.uk/hotels/travel/lpllp-liverpool-marriott-hotel-city-centre/" target="_blank"><h5 class="underline">Liverpool Marriott Hotel City Centre</h5></a> <h6>One Queen Square, Liverpool, L1 1RH</h6> <p>Tel: 0151 476 8000</p>'
     },
     {
         location: {
             lat: 53.403892478109945, 
             lng: -2.9831572322140993
         },
-        content: "<p>premier inn liverpool city centre</p>"
+        content: '<a href="https://www.premierinn.com/gb/en/hotels/england/merseyside/liverpool/liverpool-city-centre-liverpool-one.html" target="_blank"><h5 class="underline">Premier Inn Liverpool City Centre (Liverpool One) Hotel</h5></a> <h6>48 Hanover St, Liverpool, L1 4AF</h6> <p>Tel: 0333 321 9282</p>'
     },
     {
         location: {
             lat: 53.404976789410135, 
             lng: -2.982064615011278
         },
-        content: "<p>quest</p>"
+        content: '<a href="https://www.questapartments.co.uk/properties/merseyside/liverpool/quest-liverpool-city-centre/hotel-deals?gclid=CjwKCAjw6fCCBhBNEiwAem5SOys4xheupaNGmcfxVOvWOQZJO0nF6R-Kpe7sJCg0KHiezzQPtjE86BoCu4AQAvD_BwE" target="_blank"><h5 class="underline">Quest Liverpool City Centre</h5></a> <h6>72 Church St, Liverpool L1 3AY</h6> <p>Tel: 0151 318 1809</p>'
     },
     {
         location: {
             lat: 53.411468594937965, 
             lng: -2.996364504516911
         },
-        content: "<p>raddisson blu</p>"
+        content: '<a href="https://www.radissonhotels.com/en-us/hotels/radisson-blu-liverpool?facilitatorId=RHGSEM&cid=a:ps+b:ggl+c:emea+i:brand+e:rdb+d:ukirwe+r:brt+f:en-US+g:ho+h:LPLZH+v:cf&gclid=CjwKCAjw6fCCBhBNEiwAem5SOz7wYn3DaSmJ-tXVf0Zdwt5cOkbof_St1nv2mlVmiFxmLYmdbg8pwBoCI-AQAvD_BwE&gclsrc=aw.ds" target="_blank"><h5 class="underline">Radisson Blu Hotel Liverpool</h5></a> <h6>107 Old Hall St, Liverpool, L3 9BD</h6> <p>Tel: 0151 966 1500</p>'
     },
     {
         location: {
             lat: 53.39786420543189, 
             lng: -2.9893835591946236
         },
-        content: "<p>staybridge suites</p>"
+        content: '<a href="https://www.guestreservations.com/staybridge-suites-liverpool/booking?gclid=CjwKCAjw6fCCBhBNEiwAem5SO-ewUJRnUNnzDMVVuHr0vEWWS682qymiWu__32Im3Iby6ZA8yaY6MhoCYcMQAvD_BwE" target="_blank"><h5 class="underline">Staybridge Suites Liverpool</h5></a> <h6>21 Keel Wharf, Liverpool, L3 4FN</h6> <p>Tel: 0151 703 9700</p>'
     },
     {
         location: {
             lat: 53.40280038973694, 
             lng: -2.9806678726845903
         },
-        content: "<p>the resident liverpool </p>"
+        content: '<a href="https://www.residenthotels.com/the-resident-liverpool/" target="_blank"><h5 class="underline">The Resident Liverpool</h5></a> <h6>29 Seel St, Liverpool, L1 4AU</h6> <p>Tel: 0151 705 2626</p>'
     },
     {
         location: {
             lat: 53.41065102729441,  
             lng: -2.9863993907791606
         },
-        content: "<p>the richmond apart-hotel</p>"
+        content: '<a href="https://richmondliverpool.com/" target="_blank"><h5 class="underline">The Richmond Apart-Hotel</h5></a> <h6>24 Hatton Garden, Liverpool, L3 2AA</h6> <p>Tel: 0151 236 1220</p>'
     },
 ];
 var restMarkers = [
