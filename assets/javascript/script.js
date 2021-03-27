f9724aae3714927b0896d7d020a0e9e47fc0f80f
taybro23 <taybro343@outlook.com>
2021-03-27 20:51:28 +0000

    scroll button fixed. unneeded html removed.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,13 +1,23 @@
 
-// scroll to top button. code followed from youtube video - https://www.youtube.com/watch?v=FK5DEa1Hvco
+// scroll to top button. code followed from youtube video and w3s - https://www.youtube.com/watch?v=FK5DEa1Hvco / https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
 
 const btnScroll = document.querySelector("#btnscroll");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    btnScroll.style.display = "block";
+  } else {
+    btnScroll.style.display = "none";
+  };
+};
 
 btnScroll.addEventListener("click", function () {
     window.scrollTo({
         top: 0,
         left: 0,
-        behaviour: "smooth"
+        behaviour: "smooth",
     });
 });
 
