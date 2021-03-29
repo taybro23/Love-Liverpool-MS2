cc9e228c3f55f042b5e0d5eef0ffa9034eb27407
taybro23 <taybro343@outlook.com>
2021-03-29 19:23:13 +0000

    removed unneccesary js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -28,33 +28,3 @@ $(document).ready(function() {
         $("nav").toggleClass("scrolled", $(this).scrollTop() > 100);
     });
 });
-
-// places of interest buttons, walkthrough for jquery from CI used // 
-
-/*$(document).ready(function (){
-
-    $("#fombtn1").click(function(){
-        $("#fomt1").toggle(); 
-    });
-
-    $("#fombtn2").click(function(){
-        $("#fomt2").toggle(); 
-    });
-
-    $("#fombtn3").click(function(){
-        $(".fomt3").toggle(); 
-    });
-    
-    $("#fombtn4").click(function(){
-        $(".fomt4").toggle(); 
-    });
-
-    $("#fombtn5").click(function(){
-        $(".fomt5").toggle(); 
-    });
-
-    $("#fombtn6").click(function(){
-        $(".fomt6").toggle(); 
-    });
-
-});*/
