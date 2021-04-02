b29b4aca9c9b36b6b8b3d47395838b3a3cffdde7
taybro23 <taybro343@outlook.com>
2021-04-02 15:05:52 +0000

    scroll to top button coded, comments added

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,3 +6,31 @@ $(document).ready(function() {
         $("nav").toggleClass("scrolled", $(this).scrollTop() > 100);
     });
 });
+
+// scroll to top button, help from youtube video and w3s - https://www.youtube.com/watch?v=FK5DEa1Hvco / https://www.w3schools.com/howto/howto_js_scroll_to_top.asp 
+
+const scrollBtn = document.querySelector("#scroll-btn");
+
+window.onscroll = function () {
+    scrollFunction();
+};
+
+// function for hiding/showing scroll to top button 
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        scrollBtn.style.display = "block";
+    } else {
+        scrollBtn.style.display = "none";
+    }
+};
+
+// function for when scroll to top button is clicked
+
+scrollBtn.addEventListener("click", function() {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behaviour: "smooth"
+    });
+});
